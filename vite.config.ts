497b4753778d3ed428aa131901e96d7036cf4cc5
alextaber <alex.taber0@gmail.com>
2022-05-08 10:51:08 -0400

    implement electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from "vite-tsconfig-paths";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: path.resolve(__dirname, "./dist/"),
   plugins: [vue(), tsconfigPaths()],
 })
